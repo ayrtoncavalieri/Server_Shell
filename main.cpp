eaa5e5e9978bb99e799815b5a58d1108b2cac7d4
Ayrton Cavalieri <ayrtoncavalieri@gmail.com>
2020-04-04 19:08:24 -0300

    Removed unecessary commentaries.

--- main.cpp
+++ main.cpp
@@ -4,20 +4,4 @@
 #include "PocoInclude.hpp"
 #include "WebSocketServer.hpp"
 
-/*using namespace std;
-
-int main(int argc, const char *argv[])
-{
-    try{
-        Server srv(9980);
-        cout << "OK\n";
-        srv.startSrv();
-        srv.supportIPv6() == true ? cout << "IPv6\n" : cout << "~IPv6\n";
-    }catch(Poco::IllegalStateException e){
-        cout << e.what();
-    }
-    
-    return 0;
-}*/
-
 POCO_SERVER_MAIN(WebSocketServer);
