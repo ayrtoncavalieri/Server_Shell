811797e5fafc1fb64f69564b3b720688085eee54
Ayrton Cavalieri <ayrtoncavalieri@gmail.com>
2020-07-18 13:27:03 -0300

    Implemented commonOps.

---
+++ commonOps.cpp
@@ -0,0 +1,63 @@
+#include "commonOps.hpp"
+
+std::string commonOps::passwordCalc(std::string pass, std::string salt, std::string rSalt)
+{
+    pass += salt;
+    pass += rSalt;
+    return commonOps::SHA3Wrapper(pass);
+}
+
+std::string commonOps::SHA3Wrapper(std::string str)
+{
+    std::string result;
+
+    Poco::Crypto::DigestEngine eng("SHA3-256");
+    eng.update(str);
+    result = eng.digestToHex(eng.digest());
+    Poco::toUpperInPlace(result);
+
+    return result;
+}
+
+std::string commonOps::genRsalt(unsigned int tam)
+{
+    std::string rSalt = "";
+
+    char ch;
+    for(unsigned int i = 0; i < tam; ++i){
+        do{
+            ch = (char)rand();
+        }while(ch < 33 || ch > 126);
+        rSalt += ch;
+    }
+
+    return rSalt;
+}
+
+std::string commonOps::genAuthID(unsigned int tam)
+{
+    std::string resp = "";
+    char val;
+    for(unsigned int i = 0; i < tam; ++i){
+        do{
+            val = (char)rand();
+        }while(!(val >= '0' && val <= '9') && !(val >= 'A' && val <= 'Z') && !(val >= 'a' && val <= 'z'));
+        resp += val;
+    }
+    return resp;
+}
+
+Object::Ptr commonOps::erroOpJSON(int op, std::string erro)
+{
+    Poco::JSON::Object::Ptr respJSON = new Poco::JSON::Object;
+    respJSON->set("status", Poco::NumberFormatter::format(op + 50));
+    respJSON->set("error", erro);
+    return respJSON;
+}
+
+void commonOps::logMessage(std::string who, std::string what, Message::Priority howBad)
+{
+    Message msn(who, what, howBad);
+    Application& app = Application::instance();
+    app.logger().log(msn);
+}
---
+++ commonOps.hpp
@@ -0,0 +1,23 @@
+#ifndef COMMOPS_H
+#define COMMOPS_H
+
+#include "PocoInclude.hpp"
+#include <cstdlib>
+#include <Poco/NumberFormatter.h>
+#include <Poco/JSON/Object.h>
+
+using namespace Poco::JSON;
+
+class commonOps{
+    public:
+        static std::string passwordCalc(std::string pass, std::string salt, std::string rSalt = "");
+        static std::string genRsalt(unsigned int tam);
+        static std::string genAuthID(unsigned int tam);
+        static Object::Ptr erroOpJSON(int op, std::string erro);
+        static void logMessage(std::string who, std::string what, Message::Priority howBad);
+    private:
+        static std::string SHA3Wrapper(std::string str);
+
+};
+
+#endif
