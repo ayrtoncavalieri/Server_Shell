f8e662282747adaad95dd3039d72ed191063b474
Ayrton Cavalieri <ayrtoncavalieri@gmail.com>
2020-07-18 13:04:28 -0300

    Implemented logging.

--- ReqHandlerFactory.cpp
+++ ReqHandlerFactory.cpp
@@ -3,20 +3,24 @@
 HTTPRequestHandler* RequestHandlerFactory::createRequestHandler(const HTTPServerRequest &request)
 {
     Application& app = Application::instance();
-    app.logger().information("Request from "
+    Message msn("ReqHandlerFactory", "Request from "
         + request.clientAddress().toString()
         + ": "
         + request.getMethod()
         + " "
         + request.getURI()
         + " "
-        + request.getVersion());
-
+        + request.getVersion(), Message::PRIO_INFORMATION);
+    app.logger().log(msn);
+#ifdef TRACE
     for (HTTPServerRequest::ConstIterator it = request.begin(); it != request.end(); ++it)
     {
-        app.logger().information(it->first + ": " + it->second);
+        msn.setSource("ReqHandlerFactory");
+        msn.setText(it->first + ": " + it->second);
+        msn.setPriority(Poco::Message::PRIO_DEBUG);
+        app.logger().log(msn);
     }
-
+#endif
     if(request.find("Upgrade") != request.end() && Poco::icompare(request["Upgrade"], "websocket") == 0
     && request.find("sec-websocket-protocol") != request.end() && Poco::icompare(request["sec-websocket-protocol"], "PDRAUM") == 0)
         return new WebSocketRequestHandler;
