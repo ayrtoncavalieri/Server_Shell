79f1eafa39caf2f21778bc8e9d25db73a93ec30f
Ayrton Cavalieri <ayrtoncavalieri@gmail.com>
2020-07-28 14:11:47 -0300

    Modifications in exception treatment and JSON parsing.

--- ServerOps.cpp
+++ ServerOps.cpp
@@ -14,40 +14,52 @@ std::string ServerOps::processReq(std::string &req)
 {
     Application& app = Application::instance();
     std::string respJSON;
-    Poco::JSON::Object::Ptr reqJSON, procJSON;
+    Poco::JSON::Object::Ptr reqJSON, procJSON;  
     Poco::JSON::Parser p;
-    unsigned int option;
+    unsigned int option = 0;
     //Process data
     try{
-        option = Poco::NumberParser::parseUnsigned(req.substr(0, 3));
         reqJSON = p.parse(req.substr(3, std::string::npos)).extract<Poco::JSON::Object::Ptr>();
+        option = reqJSON->getValue<unsigned int>("option");
 
         //Select option and do something.
 
-        std::ostringstream streamg;
-        procJSON->stringify(streamg, 0, -1);
-        respJSON = streamg.str();
+    }catch(Poco::InvalidAccessException &e){
+        commonOps::logMessage("ServerOps::processReq", "JSON InvalidAccess", Message::PRIO_WARNING);
+        procJSON = commonOps::erroOpJSON(option, "json_with_error");
+    }catch(Poco::RangeException &e){
+        commonOps::logMessage("ServerOps::processReq", "JSON RangeException", Message::PRIO_WARNING);
+        procJSON = commonOps::erroOpJSON(option, "json_with_error");
     }catch(Poco::JSON::JSONException &e){
-        Message syntexc("ServerOps::processReq", "JSON with error", Message::PRIO_WARNING);
-        app.logger().log(syntexc);
-        respJSON = "{\"status\": \"" + req.substr(0, 3) + "\", \"error\":\"json_with_error\"}";
-    }catch(Poco::SyntaxException &e){
-        Message syntexc("ServerOps::processReq", "Number syntax error: " + req.substr(0, 3), Message::PRIO_WARNING);
-        app.logger().log(syntexc);
-        respJSON = "{\"status\": \"" + req.substr(0, 3) + "\", \"error\":\"wrong_format\"}";
+        commonOps::logMessage("ServerOps::processReq", "JSON with error", Message::PRIO_WARNING);
+        procJSON = commonOps::erroOpJSON(option, "json_with_error");
+    }catch(Poco::Data::DataException &e){
+        std::string reason = "Poco DataException: ";
+        reason += e.className();
+        reason += " -> ";
+        reason += e.message();
+        commonOps::logMessage("ServerOps::processReq", reason, Message::PRIO_CRITICAL);
+        procJSON = commonOps::erroOpJSON(option, "data_exception");
     }catch(Poco::Exception &e){
         std::string reason = "Poco Exception: ";
         reason += e.className();
         reason += " -> ";
         reason += e.message();
-        Message unk("ServerOps::processReq", reason, Message::PRIO_CRITICAL);
-        app.logger().log(unk);
-        respJSON = "{\"status\": \"" + req.substr(0, 3) + "\", \"error\":\"unknown_exception\"}";
+        commonOps::logMessage("ServerOps::processReq", reason, Message::PRIO_CRITICAL);
+        procJSON = commonOps::erroOpJSON(option, "server_exception");
+    }catch(std::exception &e){
+        std::string reason = "std::exception";
+        reason += " -> ";
+        reason += e.what();
+        commonOps::logMessage("ServerOps::processReq", reason, Message::PRIO_CRITICAL);
+        procJSON = commonOps::erroOpJSON(option, "std_exception");
     }catch( ... ){
-        Message unk("ServerOps::processReq", "Unknown exception!", Message::PRIO_CRITICAL);
-        app.logger().log(unk);
-        respJSON = "{\"status\": \"" + req.substr(0, 3) + "\", \"error\":\"unknown_exception\"}";
+        commonOps::logMessage("ServerOps::processReq", "Unknown exception!", Message::PRIO_CRITICAL);
+        procJSON = commonOps::erroOpJSON(option, "unknown_exception");
     }
     
+    std::ostringstream streamg;
+    procJSON->stringify(streamg, 0, -1);
+    respJSON = streamg.str();
     return respJSON;
 }
--- ServerOps.hpp
+++ ServerOps.hpp
@@ -2,12 +2,15 @@
 #define SERVEROPS_H
 
 #include <iostream>
+#include <exception>
 #include "PocoInclude.hpp"
+#include "commonOps.hpp"
 #include <Poco/NumberParser.h>
 #include <Poco/NumberFormatter.h>
 #include <Poco/JSON/Parser.h>
 #include <Poco/JSON/Object.h>
 #include <Poco/JSON/Array.h>
+#include <Poco/Data/DataException.h>
 
 class ServerOps
 {
