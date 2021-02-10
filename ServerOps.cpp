dec1fde36ac3d4070627cdaa35a01c65a552191c
Ayrton Cavalieri de Almeida <ayrtoncavalieri@gmail.com>
2021-02-10 00:27:02 -0300

    Added GPL license

--- ServerOps.cpp
+++ ServerOps.cpp
@@ -1,3 +1,20 @@
+/*
+    This file is part of Server_Shell.
+
+    Server_Shell is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Server_Shell is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Server_Shell.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "ServerOps.hpp"
 
 ServerOps::ServerOps(): salt("S417_3*ampl3")
